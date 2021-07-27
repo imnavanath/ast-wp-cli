0162b6c86b0e33e87c90e8ceb3ee3c1c490c803f
imnavanath <navanathb@bsf.io>
2021-07-27 14:39:53 +0530

    feat: Added CLI command which will update theme-auto-versio from Astra Settings

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ast-wp-cli
-&lt;code>&lt;strong>wp astra update_auto_version&lt;/strong>&lt;/code> Command will update 'theme-auto-version' from Astra Settings to 3.4.5.
+# AST WP_CLI #
+**Contributors:** [navanathbhosale](https://profiles.wordpress.org/navanathbhosale)  
+
+"wp astra confirm" will inform you that "astra" wp-cli command is in action.
+"wp astra update_auto_version" Command will update 'theme-auto-version' from Astra Settings to 3.4.5.
---
+++ ast-wp-cli.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Plugin Name:     Astra WP_CLI
+ * Description:     <code><strong>wp astra update_auto_version</strong></code> Command will update 'theme-auto-version' from Astra Settings to 3.4.5.
+ * Author:          Navanath Bhosale
+ * Text Domain:     'ast-wp-cli'
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         WP_CLI
+ */
+
+if ( ! class_exists( 'Astra_Update_Auto_Version_WP_CLI' ) && class_exists( 'WP_CLI_Command' ) ) {
+
+    class Astra_Update_Auto_Version_WP_CLI extends WP_CLI_Command {
+
+        /**
+		 * Use: Update 'theme-auto-version' from 'astra-settings'.
+		 *
+		 * @since x.x.x
+		 * @param  array $args       Arguments.
+		 * @param  array $assoc_args Associated Arguments.
+		 * @return void.
+		 */
+		public function update_auto_version( $args = array(), $assoc_args = array() ) {
+
+			$astra_settings = get_site_option( 'astra-settings' );
+
+            if( isset( $astra_settings['theme-auto-version'] ) ) {
+                WP_CLI::line( 'Before update - ' . $astra_settings['theme-auto-version'] );
+
+                // Updating theme-auto-version here = 3.4.5.
+                $astra_settings['theme-auto-version'] = '3.4.5';
+
+                WP_CLI::line( 'After update - ' . $astra_settings['theme-auto-version'] );
+
+                update_site_option( 'astra-settings', $astra_settings );
+            }
+		}
+
+        /**
+         * Just confirming the command.
+         */
+        public function confirm( $args, $assoc_args ) {      
+            WP_CLI::line( 'Great! CLI command is working!' );
+        }
+    }
+
+    WP_CLI::add_command( 'astra', 'Astra_Update_Auto_Version_WP_CLI' );
+}
